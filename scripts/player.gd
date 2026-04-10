f2b8f1259bbdba42f89ae71dc8686c78ca8a9f44
silentnight-sparklerain <964476541@qq.com>
2026-04-10 16:38:15 +0800

    更新玩家机制、动画

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,22 +1,40 @@
 extends CharacterBody2D
 
 
-const SPEED = 100.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 150.0  # 速度
+const JUMP_VELOCITY = -300.0  # 跳跃速度
 
+# 获取对AnimatedSprite2D的引用
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# 玩家没有站在平台上，就增加重力
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# 处理跳跃操作
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# 获取移动方向：定义了一个基于输入的方向变量，不按按钮则值为0，按左则为-1，按右则为1
+	var direction := Input.get_axis("move_left", "move_right")  
+	
+	# 左右移动的时候翻转动画
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+	
+	# 控制动画播放
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		animated_sprite.play("jump")
+	
+	# 移动
 	if direction:
 		velocity.x = direction * SPEED
 	else:
